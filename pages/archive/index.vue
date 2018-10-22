f9180f798422d07cf36626fb23159ae2c768b347
Josh Sanderson <josh@jgstechnical.com>
2018-10-22 13:57:25 -0400

    Add 2009 site

--- pages/archive/index.vue
+++ pages/archive/index.vue
@@ -4,22 +4,20 @@
       <div class="col-12">
         <h1>Archive</h1>
         <p class="lead">
-          Old Websites & Projects
+          Old websites & projects. More of these are snapshots, so not all links and functionality will work.
         </p>
       </div>
     </div>
     <div class="row">
       <div class="col">
-        <h3>~2010-2018
-          <small>(snapshots, so not all links and functionality will work)</small>
-        </h3>
+        <h3>~2010-2018</h3>
         <ul>
           <li>
-            <a href="https://archive.kaliatech.com/2018/websites/www.jgstechnical.com/index.html">jgstechnical.com</a>
+            <a href="https://archive.kaliatech.com/2018/websites/www.jgstechnical.com/index.html">jgstechnical.com (2018)</a>
             - Previous business website
           </li>
           <li>
-            <a href="https://archive.kaliatech.com/2018/websites/www.kaliatech.com/index.html">kaliatech.com</a> -
+            <a href="https://archive.kaliatech.com/2018/websites/www.kaliatech.com/index.html">kaliatech.com (2018)</a> -
             Previous personal website. Wordpress with custom theme and plugins.
             <ul>
               <li>
@@ -35,8 +33,19 @@
             </ul>
           </li>
           <li>
-            <a href="http://archive.kaliatech.com/2018/websites/www.flightregression.com/index.html">flightregression.com</a>
+            <a href="http://archive.kaliatech.com/2018/websites/www.flightregression.com/index.html">flightregression.com (2011)</a>
             - Old project website. Became defunct once google decided to discontinue their Google Earth Plugin.
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h3>~2002-2009</h3>
+        <ul>
+          <li>
+            <a href="https://archive.kaliatech.com/2018/websites/www.jgstechnical.com/archive_20090412/index.html">jgstechnical.com (2009)</a>
+            - Previous business website
           </li>
         </ul>
       </div>
