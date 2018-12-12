fb3e7b57493c1e6ca63b90ee747f7e12f04543d2
Josh Sanderson <josh@jgstechnical.com>
2018-12-12 09:54:26 -0500

    Add old sql-extract-filename post

---
+++ pages/blog/2010/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2>2010</h2>
+      </div>
+    </div>
+    <blog-posts year="2010"/>
+  </div>
+</template>
+<script>
+import BlogPosts from '~/components/BlogPosts'
+
+export default {
+  components: {BlogPosts},
+  head () {
+    return {
+      title: 'Blog - 2010 | Kaliatech'
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="scss">
+</style>
