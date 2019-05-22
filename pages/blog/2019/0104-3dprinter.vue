24ed178cfb03354a78dc19fc1baa7f0fa8aac66f
Josh Sanderson <josh@jgstechnical.com>
2019-05-22 15:15:13 -0400

    Add 3D printer update post

--- pages/blog/2019/0104-3dprinter.vue
+++ pages/blog/2019/0104-3dprinter.vue
@@ -3,27 +3,25 @@
     <div class="row">
       <div class="col">
         <p>
-          I purchased a 3D printer "for the kids" over the holiday. It's a
+          I purchased a 3D printer &quot;for the kids&quot; over the holiday. It's a
           <a href="https://www.prusa3d.com/original-prusa-i3-mk3/">Prusa i3 MK3</a>
-          . Purchased as a kit and had to be assembled. Hard to justify practically, but regardless, it's really
-          interesting.
+          . Purchased as a kit and had to be assembled. Hard to justify practically, but it's <i>really</i> interesting.
+        </p>
+        <p>
+          For more photos, see update: <nuxt-link to="/blog/2019/0521-3dprinter-update">2019/0521-3dprinter-update</nuxt-link>
         </p>
       </div>
     </div>
 
-
     <h2>Photos</h2>
     <div class="row">
-      <div v-for="n in 10" :key="n" class="col-4">
-        <a :href="'/i/blog/2019/3dprinter_' + n + '.jpg'">
+      <div v-for="n in 10" :key="n" class="col-sm-4">
+        <a :href="'/i/blog/2019/0104-3dprinter/3dprinter_' + n + '.jpg'">
           <figure class="figure">
-            <img :src="'/i/blog/2019/3dprinter_' + n + '.jpg'"
-                 class="figure-img img-fluid rounded">
+            <img :src="'/i/blog/2019/0104-3dprinter/3dprinter_' + n + '.jpg'" class="figure-img img-fluid rounded">
           </figure>
         </a>
       </div>
     </div>
   </BlogPost>
 </template>
-
-
