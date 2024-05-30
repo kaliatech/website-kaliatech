484ccaafc802272f1f0672ffaad66e34d980f918
Josh Sanderson <josh@jgstechnical.com>
2024-05-30 14:59:41 -0400

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,24 +24,24 @@ export default defineConfig({
       chunkSizeWarningLimit: 2495,
       // rollupOptions: {
       //   output: {
-          // This does not seem to work....
-          //manualChunks: {
-          //   mermaid: ['mermaid', 'katex']
-          // }
-          // This results in correct chunk, but chunk gets loaded on every page, not just when needed. See discussion
-          // in discord.
-          // manualChunks: (id, { getModuleInfo, getModuleIds }) => {
-          //   const moduleInfo = getModuleInfo(id)
-          //   if (
-          //     id.includes('/mermaid/') ||
-          //     moduleInfo.dynamicImporters.some(
-          //       (importer) =>
-          //         importer.includes('/mermaid/') ||
-          //         moduleInfo.importers.some((importer) => importer.includes('/mermaid/')),
-          //     )
-          //   ) {
-          //     return 'mermaid'
-          //   }
+      // This does not seem to work....
+      //manualChunks: {
+      //   mermaid: ['mermaid', 'katex']
+      // }
+      // This results in correct chunk, but chunk gets loaded on every page, not just when needed. See discussion
+      // in discord.
+      // manualChunks: (id, { getModuleInfo, getModuleIds }) => {
+      //   const moduleInfo = getModuleInfo(id)
+      //   if (
+      //     id.includes('/mermaid/') ||
+      //     moduleInfo.dynamicImporters.some(
+      //       (importer) =>
+      //         importer.includes('/mermaid/') ||
+      //         moduleInfo.importers.some((importer) => importer.includes('/mermaid/')),
+      //     )
+      //   ) {
+      //     return 'mermaid'
+      //   }
       //   },
       // },
     },
